b8f49cae212c8c99399fe3a4038fb0628b0f720b
Raffi Khatchadourian <rkhatchadourian@citytech.cuny.edu>
2015-07-24 20:35:05 +0100

    Re-adding UI.

---
+++ edu.cuny.citytech.analyzecommonality.ui/src/edu/cuny/citytech/analyzecommonality/ui/AnalyzeStructuralCommonalityHandler.java
@@ -0,0 +1,39 @@
+package edu.cuny.citytech.analyzecommonality.ui;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.internal.ui.util.SelectionUtil;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import edu.cuny.citytech.analyzecommonality.core.analysis.StructuralCommonalityAnalyzer;
+import edu.cuny.citytech.analyzecommonality.core.model.JavaElementSet;
+
+@SuppressWarnings("restriction")
+public class AnalyzeStructuralCommonalityHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		ISelection selection = HandlerUtil.getCurrentSelectionChecked(event);
+		List<?> list = SelectionUtil.toList(selection);
+		Set<IJavaElement> elements = list.stream().filter(e -> e instanceof IJavaElement).map(e -> (IJavaElement) e)
+				.collect(Collectors.toSet());
+		JavaElementSet set = new JavaElementSet(elements, "some bug", "security");
+		
+		StructuralCommonalityAnalyzer analyzer = new StructuralCommonalityAnalyzer(1);
+		try {
+			analyzer.analyze(set, new NullProgressMonitor(), null);
+		} catch (Exception e) {
+			throw new ExecutionException(null, e);
+		}
+
+		return null;
+	}
+}
